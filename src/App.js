08ca4290a512aaa048aff5762f9140c173bc1eaa
nasichunamin <nasichunamin004@gmail.com>
2022-04-04 11:35:20 +0700

    React Deployment

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Selamat Datang di Website <br/>
+          Moch. Nasichun Amin
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/nasichunamin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Saya
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
